f5c6a42b0f506afbedcaed0730e0cf305dd6c9ca
ZafarKamal123 <sk4915497@gmail.com>
2021-12-15 14:41:22 +0500

    use post method

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const express = require('express'),
 
 app.use(express.json());
 
-app.get("/", async (request, response) => {
+app.post("/", async (request, response) => {
     
     const browser = await puppeteer.launch({
         args: [
